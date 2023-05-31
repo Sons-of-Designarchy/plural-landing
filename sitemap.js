3abf5c7fddeaa07680363b0a04c4481985912200
EdwardMD <Edwardmi444@gmail.com>
2023-05-30 19:22:47 -0600

    full migration of all blog section, and tags/ hot fix for minor problems

--- sitemap.js
+++ sitemap.js
@@ -50,5 +50,81 @@ module.exports = {
     '404': {
         title: "Page not found",
         main: '404'
-    }
+    },
+    'blog-resico': {
+        title: "Resico - La hora SAT® 🤓 ",
+        main: 'resico/index',
+    },
+    'blog-regimenes': {
+        title: "Regimenes - La hora SAT® 🤓 ",
+        main: 'regimenes/index',
+    },
+    'blog-opinion': {
+        title: "Opinion - La hora SAT® 🤓 ",
+        main: 'opinion/index',
+    },
+    'blog-pedir-facturas': {
+        title: "Como pedir tus facturas - La hora SAT® 🤓 ",
+        main: 'pedir-facturas/index',
+    },
+    'blog-evadir-al-sat': {
+        title: "Evadir al SAT? - La hora SAT® 🤓 ",
+        main: 'evadir/index',
+    },
+    'blog-IVA': {
+        title: "IVA - La hora SAT® 🤓 ",
+        main: 'iva/index',
+    },
+    'blog-deducciones-tipos': {
+        title: " Tipos de deducciones - La hora SAT® 🤓 ",
+        main: 'deducciones/index',
+    },
+    'blog-ISR': {
+        title: "ISR - La hora SAT® 🤓 ",
+        main: 'isr/index',
+    },
+    'blog-pagan-isr': {
+        title: "Ingresos en ISR - La hora SAT® 🤓 ",
+        main: 'ingresos-isr/index',
+    },
+    'blog-glosario': {
+        title: "Glosario - La hora SAT® 🤓 ",
+        main: 'glosario/index',
+    },
+    'regimenes-fiscales': {
+        title: "regimenes-fiscales - La hora SAT® 🤓 ",
+        main: 'regimenes-fiscales/index',
+    },
+    'tags': {
+        title: "tags - La hora SAT® 🤓 ",
+        main: 'tags/index',
+    },
+    'tags-deducciones': {
+        title: "tags deducciones - La hora SAT® 🤓 ",
+        main: 'tags-deducciones/index',
+    },
+    'tags-impuestos': {
+        title: "tags impuestos - La hora SAT® 🤓 ",
+        main: 'tags-impuestos/index',
+    },
+    'tags-dudas': {
+        title: "tags dudas - La hora SAT® 🤓 ",
+        main: 'tags-dudas/index',
+    },
+    'tags-portal-del-sat': {
+        title: "tags portal del sat  - La hora SAT® 🤓 ",
+        main: 'tags-portal-del-sat/index',
+    },
+    'tags-regimenes-fiscales': {
+        title: "tags regimenes fiscales  - La hora SAT® 🤓 ",
+        main: 'tags-regimenes-fiscales/index',
+    },
+    'terminos-y-condiciones': {
+        title: "Terminos y condiciones  - La hora SAT® 🤓 ",
+        main: 'terminos-y-condiciones/index',
+    },
+    'aviso-de-privacidad': {
+        title: "Aviso de privacidad  - La hora SAT® 🤓 ",
+        main: 'aviso-de-privacidad/index',
+    },
 }
