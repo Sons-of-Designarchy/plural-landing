6210e2b4a241fe82daa75ed5dd5bd6a4a82e99fe
Daniel Pliego <daniel@sleeksky.com>
2023-06-23 16:31:16 -0600

    landing page without styles (WIP), all images added, navbar and footer working (mobile version in process)

--- sitemap.js
+++ sitemap.js
@@ -2,129 +2,23 @@
 
 module.exports = {
     '': {
-        title: "La hora SAT® 🤓 ",
+        title: "Plural",
         main: 'home/index',
         header: 'home/header',
         navColor: 'light'
     },
-    'cursos': {
-        title: "Cursos - La hora SAT® 🤓 ",
-        main: 'cursos/index',
+    'music': {
+        title: "Musica-video",
+        main: 'musica_video/index',
     },
-    'curso-basico-lhs': {
-        title: "Curso Basico - La hora SAT® 🤓 ",
-        main: 'curso-basico-lhs/index',
+    'shows': {
+        title: "Musica-video",
+        main: 'show/index',
     },
-    'curso-facturacion-lhs': {
-        title: "Curso Facturacion - La hora SAT® 🤓 ",
-        main: 'curso-facturacion-lhs/index',
+    'about': {
+        title: "Musica-video",
+        main: 'about/index',
     },
-    'asesorias': {
-        title: "Asesorias - La hora SAT® 🤓 ",
-        main: 'asesorias/index',
-    },
-    'asesoria-basica': {
-        title: "Asesoría basica - La hora SAT® 🤓 ",
-        main: 'asesoria-basica/index',
-    },
-    'asesoria-practica': {
-        title: "Asesoría practica - La hora SAT® 🤓 ",
-        main: 'asesoria-practica/index',
-    },
-    'asesoria-declaracion': {
-        title: "Asesoría declaracion - La hora SAT® 🤓 ",
-        main: 'asesoria-declaracion/index',
-    },
-    'testimonios': {
-        title: "Testimonios - La hora SAT® 🤓 ",
-        main: 'testimonios/index',
-    },
-    'blog': {
-        title: "Blog - La hora SAT® 🤓 ",
-        main: 'blog/index',
-    },
-    'contact': {
-        title: "Contact",
-        main: 'contact'
-    },
-    '404': {
-        title: "Page not found",
-        main: '404'
-    },
-    'blog-resico': {
-        title: "Resico - La hora SAT® 🤓 ",
-        main: 'resico/index',
-    },
-    'blog-regimenes': {
-        title: "Regimenes - La hora SAT® 🤓 ",
-        main: 'regimenes/index',
-    },
-    'blog-opinion': {
-        title: "Opinion - La hora SAT® 🤓 ",
-        main: 'opinion/index',
-    },
-    'blog-pedir-facturas': {
-        title: "Como pedir tus facturas - La hora SAT® 🤓 ",
-        main: 'pedir-facturas/index',
-    },
-    'blog-evadir-al-sat': {
-        title: "Evadir al SAT? - La hora SAT® 🤓 ",
-        main: 'evadir/index',
-    },
-    'blog-IVA': {
-        title: "IVA - La hora SAT® 🤓 ",
-        main: 'iva/index',
-    },
-    'blog-deducciones-tipos': {
-        title: " Tipos de deducciones - La hora SAT® 🤓 ",
-        main: 'deducciones/index',
-    },
-    'blog-ISR': {
-        title: "ISR - La hora SAT® 🤓 ",
-        main: 'isr/index',
-    },
-    'blog-pagan-isr': {
-        title: "Ingresos en ISR - La hora SAT® 🤓 ",
-        main: 'ingresos-isr/index',
-    },
-    'blog-glosario': {
-        title: "Glosario - La hora SAT® 🤓 ",
-        main: 'glosario/index',
-    },
-    'regimenes-fiscales': {
-        title: "regimenes-fiscales - La hora SAT® 🤓 ",
-        main: 'regimenes-fiscales/index',
-    },
-    'tags': {
-        title: "tags - La hora SAT® 🤓 ",
-        main: 'tags/index',
-    },
-    'tags-deducciones': {
-        title: "tags deducciones - La hora SAT® 🤓 ",
-        main: 'tags-deducciones/index',
-    },
-    'tags-impuestos': {
-        title: "tags impuestos - La hora SAT® 🤓 ",
-        main: 'tags-impuestos/index',
-    },
-    'tags-dudas': {
-        title: "tags dudas - La hora SAT® 🤓 ",
-        main: 'tags-dudas/index',
-    },
-    'tags-portal-del-sat': {
-        title: "tags portal del sat  - La hora SAT® 🤓 ",
-        main: 'tags-portal-del-sat/index',
-    },
-    'tags-regimenes-fiscales': {
-        title: "tags regimenes fiscales  - La hora SAT® 🤓 ",
-        main: 'tags-regimenes-fiscales/index',
-    },
-    'terminos-y-condiciones': {
-        title: "Terminos y condiciones  - La hora SAT® 🤓 ",
-        main: 'terminos-y-condiciones/index',
-    },
-    'aviso-de-privacidad': {
-        title: "Aviso de privacidad  - La hora SAT® 🤓 ",
-        main: 'aviso-de-privacidad/index',
-    },
+
+    
 }
